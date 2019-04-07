b4e227b581f419e9e8d52a08dc040b61092a50f3
yomayy <yomaywork@gmail.com>
2019-04-07 17:09:28 +0300

    First commit

---
+++ AnalogClock/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AnalogClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AnalogClock.Properties.Resources.timer;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(478, 478);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 493);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "VKL";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 493);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "VIKL";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Часы с кукушкой";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AnalogClock/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogClock
+{
+    public partial class Form1 : Form
+    {
+
+        Pen p1, p2, p3;
+        Point center;
+        Matrix m1, m2, m3;
+
+        int h, m, s;
+        float a1, a2, a3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            
+            center = new Point(0, 0);
+
+            p1 = new Pen(Color.Red, 2);
+            p2 = new Pen(Color.Black, 5);
+            p3 = new Pen(Color.Pink, 8);
+
+
+            m1 = new Matrix();
+            m2 = new Matrix();
+            m3 = new Matrix();
+
+            GetTime();
+            pictureBox1.Invalidate();
+        }
+
+        private void GetTime()
+        {
+            h = DateTime.Now.Hour;
+            m = DateTime.Now.Minute;
+            s = DateTime.Now.Second;
+
+            a1 = s * 6;
+            a2 = (float)(m * 6 + s * 0.1);
+            a3 = (float)(h * 30 + m * 0.5 + s * 0.5);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetTime();
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            
+
+            int x = pictureBox1.Width / 2;
+            int y = pictureBox1.Height / 2;
+            center = new Point(x, y);
+
+            m1.RotateAt(a1, center);
+            g.Transform = m1;
+            g.DrawLine(p1, x, y, x + 100, y + 100);
+            m1.Reset();
+
+            m2.RotateAt(a2, center);
+            g.Transform = m2;
+            g.DrawLine(p2, x, y, x + 120, y + 120);
+            m2.Reset();
+
+            m3.RotateAt(a3, center);
+            g.Transform = m3;
+            g.DrawLine(p3, x, y, x + 150, y + 150);
+            m3.Reset();
+
+
+
+
+        }
+    }
+}
